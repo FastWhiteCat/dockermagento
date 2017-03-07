23713fbb7f950a2d125991c667cf3ea592b99af5
Łukasz Olszewski <asdluki@gmail.com>
2017-03-07 16:29:56 +0100

    Update setup.sh

--- app/setup.sh
+++ app/setup.sh
@@ -8,15 +8,13 @@ if [ ! -d "$DIR" ]; then
 	mkdir $DIR
 fi
 
-if [ ! -d "$(ls -A $DIR)" ]; then
-	echo "Magento install"
-	cp /tmp/auth.json ~/.composer/auth.json
-	composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition $DIR
-	cd $DIR
-	find var vendor pub/static pub/media app/etc -type f -exec chmod u+w {} \;
-	find var vendor pub/static pub/media app/etc -type d -exec chmod u+w {} \;
-	chmod u+x bin/magento
-fi
+echo "Magento install"
+cp /tmp/auth.json ~/.composer/auth.json
+composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition $DIR
+cd $DIR
+find var vendor pub/static pub/media app/etc -type f -exec chmod u+w {} \;
+find var vendor pub/static pub/media app/etc -type d -exec chmod u+w {} \;
+chmod u+x bin/magento
 
 php-fpm
 
